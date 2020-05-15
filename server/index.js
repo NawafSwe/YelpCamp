e2a42225fb52988ff6fe89217ebe9394b40b5a18
nawaf <princenawaf99@hotmail.com>
2020-05-15 03:14:42 +0300

    setting campgrounds page and add fake data

--- server/index.js
+++ server/index.js
@@ -19,3 +19,45 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
+
+
+/*---------------------------- app routes ---------------------------- */
+
+/* this route is the initial route where render the home page*/
+app.get('/', (req, res) => {
+    res.render('home');
+});
+ 
+/* this route is going to show all campgrounds info
+    campgrounds are lists of the format 
+    [
+    {name:name,
+    image_url: url
+    }
+    ]
+
+    we will take the data from the data base later on ;; 
+
+*/
+let camp_grounds_list = [
+  {
+    name: 'ksa',
+    image_url:
+      'https://koa.com/blog/images/make-tent-camping-more-comfortable.jpg?preset=blogPhoto'
+  },
+  {
+    name: 'ksa',
+    image_url:
+      'https://koa.com/blog/images/make-tent-camping-more-comfortable.jpg?preset=blogPhoto'
+  },
+  {
+    name: 'ksa',
+    image_url:
+      'https://koa.com/blog/images/make-tent-camping-more-comfortable.jpg?preset=blogPhoto'
+  }
+]; 
+app.get('/campgrounds', (req, res) => { 
+  res.render('campgrounds', { camp_grounds_list: camp_grounds_list });
+});
+
+
